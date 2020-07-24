0d51d4bc3b5cd2282f4880469667ee92f012a938
masataka michikawa <masataaaka3@icloud.com>
2020-07-24 04:42:08 +0000

    add-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'comments/create'
+  get 'comments/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
