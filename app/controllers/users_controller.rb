b5cc88996f830a0c1a8fb3a526b2b18353fd3788
masataka michikawa <masataaaka3@icloud.com>
2020-07-25 09:44:59 +0000

    add-user_layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,19 @@ class UsersController < ApplicationController
     if @user != current_user
       redirect_to user_path(current_user), notice: "ユーザー情報を更新しました"
   end
+end
 
   def update
   end
 
   def create
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
   end
+end
+
   def user_params
     params.require(:user).permit(:name, :profile, :image)
+  end
 end
