ada62fada81afa41ce801e6a61e80d79f75af261
masataka michikawa <masataaaka3@icloud.com>
2020-07-27 12:24:20 +0000

    add-user-index-show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(prams[:id])
-    if @user != current_user
-      redirect_to usr_path(current_user), alert: '不正なアクセス'
-    end
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
     if @user != current_user
-      redirect_to user_path(current_user), notice: "ユーザー情報を更新しました"
   end
 end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'ユーザー情報を更新しました。'
+    else
+      render :edit
+    end
   end
 
   def create
@@ -27,7 +29,8 @@ end
   end
 end
 
-  def user_params
-    params.require(:user).permit(:name, :profile, :image)
-  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :profile, :image)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'homes/about'
-  resources :users do
+  resources :users, only: %i(show edit update index ) do
   resource :relationships, only: %i(create destroy)
   get 'follows' => 'relationships#follower', as:'follows'
   get 'followers' => 'relationships#followed',as: 'followers'
