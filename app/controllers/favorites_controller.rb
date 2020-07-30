d72b0287eb7ff87943a6c35a1be7d79b2ad5c95d
masataka michikawa <masataaaka3@icloud.com>
2020-07-30 07:35:06 +0000

    add-like

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,15 @@
 class FavoritesController < ApplicationController
   def create
+  	user = User.find(params[:user_id])
+    favorite = current_user.favorites.new(user_id: user.id)
+    favorite.save
+    redirect_to user_path(user)
   end
 
   def destroy
+  	user = User.find(params[:user_id])
+    favorite = current_user.favorites.find_by(user_id: user.id)
+    favorite.destroy
+    redirect_to user_path(user)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :comments, dependent: :destroy
-  has_many :favorites, dependent: :destroy
   has_many :follower, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :followed, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following_user, through: :follower, source: :followed
   has_many :follower_user, through: :followed, source: :follower
   mount_uploader :image, ImageUploader
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   # フォロー機能
   def favorited_by?(user)
